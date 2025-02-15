015dc8c5c8e61b928cd4402628fe81afdc46e457
Ibrahim <alnajjar.ibrahim@gmail.com>
2025-02-15 17:53:03 +0100

    Apply to do library with windowsForm and connected with console library

---
+++ Multiple projects/WindowsFormsAppTry/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyFirstClass;
+
+namespace WindowsFormsAppTry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyFirstMath myFirstMath = new MyFirstMath();
+            MessageBox.Show(myFirstMath.sum(10, 13).ToString()); 
+        }
+    }
+}
