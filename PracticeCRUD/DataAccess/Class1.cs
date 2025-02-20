a3f14a866788c99db8a17bcd8d656f19a40dfa97
Ibrahim <alnajjar.ibrahim@gmail.com>
2025-02-20 21:11:46 +0100

    Create new project CRUD for oefenen

---
+++ PracticeCRUD/DataAccess/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class clsDataAccess
+    {
+        static string ConnectionString = "Server=.;Database=HR_Database;User Id=sa;Password=123456;";
+
+
+       public static string GetFirstCountryName(int CountryID)
+        {
+            string CountryName = "";
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+            string query = "select * from Countries ";
+            SqlCommand command = new SqlCommand(query, connection);
+            //  command.Parameters.AddWithValue("@CountryID", CountryID);
+            try
+            {
+                connection.Open();
+                object Result = command.ExecuteScalar();
+                if (Result != null)
+                {
+                    CountryName = Result.ToString();
+                }
+                else
+                {
+                    CountryName = "";
+                }
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+            return CountryName;
+
+        }
+    }
+}
--- PracticeCRUD/PracticeCRUD/Program.cs
+++ PracticeCRUD/PracticeCRUD/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess;
 
 namespace PracticeCRUD
 {
     internal class Program
     {
+       
+
+        
+
         static void Main(string[] args)
         {
+            Console.WriteLine(  clsDataAccess.GetFirstCountryName(4));
+            Console.ReadKey(); 
         }
     }
 }
